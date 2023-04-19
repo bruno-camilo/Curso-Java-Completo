b146e3a002cf5c2db6a69344dfb00ccf58b8ccd5
Bruno Camilo da Silva <b.camilosilva16@gmail.com>
2023-04-19 14:59:29 -0300

    Exercicios da Estrutura de repetição

---
+++ Estrutura-repeticao/EstruturasRepeticao/src/While/Exercicio02.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while(x != 0 && y != 0){
+            if(x > 0 && y > 0){
+                System.out.println("Primeiro");
+                x = sc.nextInt();
+                y = sc.nextInt();
+            } else if (x < 0 && y > 0) {
+                System.out.println("Segundo");
+                x = sc.nextInt();
+                y = sc.nextInt();
+            }else if(x < 0 && y < 0){
+                System.out.println("Terceiro ");
+                x = sc.nextInt();
+                y = sc.nextInt();
+
+            } else if (x > 0 && y < 0) {
+                System.out.println("Quarto");
+                x = sc.nextInt();
+                y = sc.nextInt();
+            }
+        }
+        sc.close();
+    }
+}
